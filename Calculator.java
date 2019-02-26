8a584984db525b2799acb4237ce2aea70f9e8705
jdchan <46948529+jdchan@users.noreply.github.com>
2019-02-25 21:18:11 -0700

    Add files via upload
    Initial version for 260 assignment 2

---
+++ Calculator.java
@@ -0,0 +1,49 @@
+
+package cse360assign3;
+
+public class Calculator {
+	/**
+	 * This calculates values
+	 * @author Jendric Shawn Dy Chan
+	 * @version 1.0
+	 * @param value inoput value
+	 * @param getHistory retrieves last entry/result
+	 * @param total total of the calculation, or the = sign on an actual calculator
+	 */
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * 
+	 * @return returns the number 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	/**
+	 * 
+	 * @return returns the last entry/result
+	 */
+	public String getHistory () {
+		return "";
+	}
+	
+}
